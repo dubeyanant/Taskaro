390f50d1da59af6d677f5480009273fa4a8de5fa
Anant Dubey <anantkumar312@gmail.com>
2023-03-02 08:35:07 +0530

    connected welcomeScreen and loginScreen

--- app/src/main/java/com/soc/taskaro/activities/WelcomeScreen.java
+++ app/src/main/java/com/soc/taskaro/activities/WelcomeScreen.java
@@ -1,16 +1,31 @@
 package com.soc.taskaro.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.soc.taskaro.R;
 
 public class WelcomeScreen extends AppCompatActivity {
 
+    Button welcomeScreenButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        welcomeScreenButton = (Button) findViewById(R.id.welcomeScreenButton);
+
+        welcomeScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), LoginScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
