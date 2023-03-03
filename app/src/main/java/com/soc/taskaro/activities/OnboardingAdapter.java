942493045f8d4683aa1ae5e22738e4aaec4b4a1e
Mayur <mayurjadhav3232@gmail.com>
2023-03-03 11:13:22 +0530

    solved merge conflict

--- app/src/main/java/com/soc/taskaro/activities/OnboardingAdapter.java
+++ app/src/main/java/com/soc/taskaro/activities/OnboardingAdapter.java
@@ -25,22 +25,23 @@ public class OnboardingAdapter extends PagerAdapter {
     };
 
     int headings[] = {
-        R.string.do_it,
-        R.string.schedule_it,
-        R.string.delegate_it,
-        R.string.delete_it,
+            R.string.do_it,
+            R.string.schedule_it,
+            R.string.delegate_it,
+            R.string.delete_it,
     };
 
     int description[] = {
-      R.string.do_it_description,
-      R.string.schedule_it_description,
-      R.string.delegate_it_description,
-      R.string.delete_it_description,
+            R.string.do_it_description,
+            R.string.schedule_it_description,
+            R.string.delegate_it_description,
+            R.string.delete_it_description,
     };
 
-    public OnboardingAdapter(Context context){
+    public OnboardingAdapter(Context context) {
         this.context = context;
     }
+
     @Override
     public int getCount() {
         return headings.length;
@@ -48,14 +49,14 @@ public class OnboardingAdapter extends PagerAdapter {
 
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-        return view==(LinearLayout) object;
+        return view == (LinearLayout) object;
     }
 
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
         LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
-        View view= layoutInflater.inflate(R.layout.onboard_resource,container, false);
+        View view = layoutInflater.inflate(R.layout.onboard_resource, container, false);
 
         ImageView slideTitleImage = (ImageView) view.findViewById(R.id.onBoard_RImageView);
         TextView sliderHeading = (TextView) view.findViewById(R.id.onBoard_RTextView2);
--- app/src/main/java/com/soc/taskaro/activities/OnboardingScreen.java
+++ app/src/main/java/com/soc/taskaro/activities/OnboardingScreen.java
@@ -1,8 +1,5 @@
 package com.soc.taskaro.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
@@ -11,9 +8,12 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 import com.soc.taskaro.R;
 
-public class activity_onboarding_screen extends AppCompatActivity {
+public class OnboardingScreen extends AppCompatActivity {
 
     ViewPager viewPager;
     LinearLayout dotsLayout;
@@ -21,6 +21,7 @@ public class activity_onboarding_screen extends AppCompatActivity {
 
     TextView[] dots;
     OnboardingAdapter onboardingAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +34,7 @@ public class activity_onboarding_screen extends AppCompatActivity {
         prev.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(getItem(0)>0){
+                if (getItem(0) > 0) {
                     viewPager.setCurrentItem(getItem(-1), true);
                 }
             }
@@ -42,10 +43,10 @@ public class activity_onboarding_screen extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(getItem(0)<3){
+                if (getItem(0) < 3) {
                     viewPager.setCurrentItem(getItem(1), true);
-                } else{
-                    Intent i = new Intent(activity_onboarding_screen.this,WelcomeScreen.class);
+                } else {
+                    Intent i = new Intent(OnboardingScreen.this, WelcomeScreen.class);
                     startActivity(i);
                     finish();
                 }
@@ -55,7 +56,7 @@ public class activity_onboarding_screen extends AppCompatActivity {
         skip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(activity_onboarding_screen.this,WelcomeScreen.class);
+                Intent i = new Intent(OnboardingScreen.this, WelcomeScreen.class);
                 startActivity(i);
                 finish();
             }
@@ -70,18 +71,18 @@ public class activity_onboarding_screen extends AppCompatActivity {
         viewPager.addOnPageChangeListener(viewLisner);
     }
 
-    public void setUpIndicator(int position){
+    public void setUpIndicator(int position) {
         dots = new TextView[4];
         dotsLayout.removeAllViews();
 
-        for(int i=0; i < dots.length; i++){
+        for (int i = 0; i < dots.length; i++) {
             dots[i] = new TextView(this);
             dots[i].setText(Html.fromHtml("&#8226"));
             dots[i].setTextSize(35);
-            dots[i].setTextColor(getResources().getColor(R.color.md_theme_light_inverseOnSurface,getApplicationContext().getTheme()));
+            dots[i].setTextColor(getResources().getColor(R.color.md_theme_light_inverseOnSurface, getApplicationContext().getTheme()));
             dotsLayout.addView(dots[i]);
         }
-        dots[position].setTextColor(getResources().getColor(R.color.md_theme_dark_shadow,getApplicationContext().getTheme()));
+        dots[position].setTextColor(getResources().getColor(R.color.md_theme_dark_shadow, getApplicationContext().getTheme()));
     }
 
     ViewPager.OnPageChangeListener viewLisner = new ViewPager.OnPageChangeListener() {
@@ -92,7 +93,7 @@ public class activity_onboarding_screen extends AppCompatActivity {
 
         @Override
         public void onPageSelected(int position) {
-            if(position>0)
+            if (position > 0)
                 prev.setVisibility(View.VISIBLE);
             else
                 prev.setVisibility(View.INVISIBLE);
@@ -104,7 +105,7 @@ public class activity_onboarding_screen extends AppCompatActivity {
         }
     };
 
-    private int getItem(int i){
-        return viewPager.getCurrentItem()+i;
+    private int getItem(int i) {
+        return viewPager.getCurrentItem() + i;
     }
 }
