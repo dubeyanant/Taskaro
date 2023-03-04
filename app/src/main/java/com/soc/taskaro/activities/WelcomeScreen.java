e69b10359f8069744c7fffc19d05b4a47742010a
Anant Dubey <anantkumar312@gmail.com>
2023-03-04 12:48:45 +0530

    centerCropped images and deleted skip button from welcomeScreen

--- app/src/main/java/com/soc/taskaro/activities/WelcomeScreen.java
+++ app/src/main/java/com/soc/taskaro/activities/WelcomeScreen.java
@@ -11,23 +11,14 @@ import com.soc.taskaro.R;
 
 public class WelcomeScreen extends AppCompatActivity {
 
-    Button skipButton, welcomeScreenButton;
+    Button welcomeScreenButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
 
-        skipButton = (Button) findViewById(R.id.skipButton);
         welcomeScreenButton = (Button) findViewById(R.id.welcomeScreenButton);
-
-        skipButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), LoginScreen.class);
-                startActivity(intent);
-            }
-        });
 
         welcomeScreenButton.setOnClickListener(new View.OnClickListener() {
             @Override
