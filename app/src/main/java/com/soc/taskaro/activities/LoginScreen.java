e44731c878315fb788344efe408c90d053ae3d8b
pareshmahajan <pareshdattatraymahajan@gmail.com>
2023-03-05 21:38:06 +0530

    Connected Login and Signup activity with transition animations

--- app/src/main/java/com/soc/taskaro/activities/LoginScreen.java
+++ app/src/main/java/com/soc/taskaro/activities/LoginScreen.java
@@ -1,16 +1,57 @@
 package com.soc.taskaro.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.soc.taskaro.R;
 
+import org.w3c.dom.Text;
+
 public class LoginScreen extends AppCompatActivity {
+    TextView txt_go_to_signup, txt_go_to_forget;
+    ImageView img_go_to_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        txt_go_to_signup = (TextView)  findViewById(R.id.txt_go_to_signup);
+        img_go_to_signup = (ImageView) findViewById(R.id.img_go_to_signup);
+
+        txt_go_to_forget = (TextView)  findViewById(R.id.txt_go_to_forget);
+
+        txt_go_to_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginScreen.this, SignUpActivity.class);
+                startActivity(i);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        });
+
+        img_go_to_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginScreen.this, SignUpActivity.class);
+                startActivity(i);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        });
+
+        txt_go_to_forget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginScreen.this, ForgetPassword.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/soc/taskaro/activities/SignUpActivity.java
+++ app/src/main/java/com/soc/taskaro/activities/SignUpActivity.java
@@ -2,15 +2,31 @@ package com.soc.taskaro.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.soc.taskaro.R;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    ImageView img_go_to_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        img_go_to_login = (ImageView) findViewById(R.id.img_go_to_login);
+
+        img_go_to_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SignUpActivity.this, LoginScreen.class);
+                startActivity(i);
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+            }
+        });
+
     }
 }
