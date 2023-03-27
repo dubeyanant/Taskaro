48f9246d07431d8ba350de5316f34011e4cc6dc0
Anant Dubey <anantkumar312@gmail.com>
2023-03-28 02:01:33 +0530

    added create task front end

--- app/src/main/java/com/soc/taskaro/CreateTaskActivity.java
+++ app/src/main/java/com/soc/taskaro/CreateTaskActivity.java
@@ -1,8 +1,20 @@
 package com.soc.taskaro;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
+
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class CreateTaskActivity extends AppCompatActivity {
 
@@ -10,5 +22,38 @@ public class CreateTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
+
+        TextView dateTextView = (TextView) findViewById(R.id.date);
+        TextView timeTextView = (TextView) findViewById(R.id.time);
+
+        dateTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MaterialDatePicker<Long> materialDatePicker = MaterialDatePicker.Builder.datePicker()
+                        .setTitleText("Select Date").setSelection(MaterialDatePicker.todayInUtcMilliseconds()).build();
+
+                materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
+                    @Override
+                    public void onPositiveButtonClick(Long selection) {
+                        String date = new SimpleDateFormat("dd/MM/yyy", Locale.getDefault()).format(new Date(selection));
+                        dateTextView.setText(MessageFormat.format("{0}", date));
+                    }
+                });
+
+                materialDatePicker.show(getSupportFragmentManager(), "tag");
+            }
+        });
+
+//        timeTextView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                MaterialTimePicker materialTimePicker = new MaterialTimePicker.Builder().setTimeFormat(TimeFormat.CLOCK_12H)
+//                        .setHour(12).setMinute(10).setTitleText("Select time").build();
+//
+//
+//
+//                materialTimePicker.show(getSupportFragmentManager(), "tag");
+//            }
+//        });
     }
 }
