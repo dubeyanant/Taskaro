c1ded675d73d4742d2da59d4ea77463139f44fa1
Anant Dubey <anantkumar312@gmail.com>
2023-03-30 17:02:10 +0530

    added done button in create task activity

--- app/src/main/java/com/soc/taskaro/CreateTaskActivity.java
+++ app/src/main/java/com/soc/taskaro/CreateTaskActivity.java
@@ -1,7 +1,9 @@
 package com.soc.taskaro;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -119,6 +121,16 @@ public class CreateTaskActivity extends AppCompatActivity {
                         subTaskLL.removeView(subTaskView);
                     }
                 });
+            }
+        });
+
+        // Return to MainActivity
+        Button createTaskDone = findViewById(R.id.createTaskDone);
+        createTaskDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+                finish();
             }
         });
     }
