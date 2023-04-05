3bbba1220962755dec088f0ff02a8a77d0425da4
Anant Dubey <anantkumar312@gmail.com>
2023-04-05 23:21:44 +0530

    added auto face change in signUp activity

--- app/src/main/java/com/soc/taskaro/activities/SignUpActivity.java
+++ app/src/main/java/com/soc/taskaro/activities/SignUpActivity.java
@@ -1,27 +1,22 @@
 package com.soc.taskaro.activities;
 
-import static android.view.WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS;
-
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Patterns;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -32,14 +27,19 @@ import com.soc.taskaro.firestore.FirestoreClass;
 import com.soc.taskaro.models.User;
 import com.soc.taskaro.utils.Extras;
 
+import java.util.Random;
 import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity {
-    LinearLayout txt_go_to_login;
-    EditText name, email, phone, password, cpassword;
-    Button btn_signup;
+    LinearLayout goToLoginLL;
+    EditText nameEditText, emailEditText, mobileEditText, passwordEditText, confirmPasswordEditText;
+    Button registerButton;
     boolean isNameValid, isEmailValid, isPhoneValid, isPasswordValid;
     ProgressDialog progressDialog;
+    ImageView faceImageView;
+    int[] face;
+    Handler handler = new Handler();
+    Random randomNumber = new Random();
     //    CheckBox showPassword;
     private FirebaseAuth firebaseAuth;
 
@@ -49,33 +49,37 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        txt_go_to_login = findViewById(R.id.txt_back);
-        name = (EditText) findViewById(R.id.txt_name);
-        email = (EditText) findViewById(R.id.txt_email);
-        phone = (EditText) findViewById(R.id.txt_mobile);
-        password = (EditText) findViewById(R.id.txt_password);
-        cpassword = (EditText) findViewById(R.id.txt_cpassword);
-        btn_signup = (Button) findViewById(R.id.btn_register);
+        goToLoginLL = findViewById(R.id.goToLoginLL);
+        nameEditText = (EditText) findViewById(R.id.nameEditText);
+        emailEditText = (EditText) findViewById(R.id.emailEditText);
+        mobileEditText = (EditText) findViewById(R.id.mobileEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        confirmPasswordEditText = (EditText) findViewById(R.id.confirmPasswordEditText);
+        registerButton = (Button) findViewById(R.id.registerButton);
 //        showPassword = (CheckBox) findViewById(R.id.showPassword);
         firebaseAuth = FirebaseAuth.getInstance();
+        faceImageView = findViewById(R.id.faceImageView);
+        face = new int[]{
+                R.drawable.vct_face_1,
+                R.drawable.vct_face_2,
+                R.drawable.vct_face_3,
+                R.drawable.vct_face_4,
+                R.drawable.vct_face_5,
+                R.drawable.vct_face_6,
+                R.drawable.vct_face_7,
+                R.drawable.vct_face_8,
+                R.drawable.vct_face_9,
+                R.drawable.vct_face_10,
+                R.drawable.vct_face_11,
+                R.drawable.vct_face_12
+        };
 
-        Window window = getWindow();
+        startCounting();
 
-        // clear FLAG_TRANSLUCENT_STATUS flag:
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
-        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-
-        // finally change the color
-        window.setStatusBarColor(ContextCompat.getColor(this, R.color.md_theme_light_secondary));
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.getDecorView().getWindowInsetsController().setSystemBarsAppearance(0, APPEARANCE_LIGHT_STATUS_BARS);
-        }
-
-        txt_go_to_login.setOnClickListener(new View.OnClickListener() {
+        goToLoginLL.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                handler.removeCallbacksAndMessages(null);
                 finish();
                 overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
             }
@@ -96,7 +100,7 @@ public class SignUpActivity extends AppCompatActivity {
 //        });
 
         //Signup Button logic
-        btn_signup.setOnClickListener(new View.OnClickListener() {
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 boolean have_WIFI = false;
@@ -121,66 +125,80 @@ public class SignUpActivity extends AppCompatActivity {
         });
     }
 
+    // Below methods are used to change face ImageView
+    private void startCounting() {
+        handler.post(run);
+    }
+
+    private Runnable run = new Runnable() {
+        @Override
+        public void run() {
+            int randNum = randomNumber.nextInt(face.length - 1);
+            faceImageView.setImageDrawable(getResources().getDrawable(face[randNum]));
+            handler.postDelayed(this, 1200);
+        }
+    };
+
     private void setValidation() {
         Pattern lowerCase = Pattern.compile("[a-z]");
         Pattern digitCase = Pattern.compile("[0-9]");
-        if (name.getText().toString().trim().isEmpty()) {
-            name.setError(getResources().getString(R.string.name_error));
+        if (nameEditText.getText().toString().trim().isEmpty()) {
+            nameEditText.setError(getResources().getString(R.string.name_error));
             isNameValid = false;
         }
-        if (!name.getText().toString().trim().isEmpty()) {
+        if (!nameEditText.getText().toString().trim().isEmpty()) {
             isNameValid = true;
         }
-        if (email.getText().toString().trim().isEmpty()) {
-            email.setError(getResources().getString(R.string.email_error));
+        if (emailEditText.getText().toString().trim().isEmpty()) {
+            emailEditText.setError(getResources().getString(R.string.email_error));
             isEmailValid = false;
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(email.getText().toString().trim()).matches()) {
-            email.setError(getResources().getString(R.string.error_invalid_email));
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailEditText.getText().toString().trim()).matches()) {
+            emailEditText.setError(getResources().getString(R.string.error_invalid_email));
             isEmailValid = false;
         } else {
             isEmailValid = true;
         }
-        if (phone.getText().toString().trim().isEmpty()) {
-            phone.setError(getResources().getString(R.string.phone_error));
+        if (mobileEditText.getText().toString().trim().isEmpty()) {
+            mobileEditText.setError(getResources().getString(R.string.phone_error));
             isPhoneValid = false;
-        } else if (phone.getText().toString().trim().length() != 10) {
-            phone.setError(getResources().getString(R.string.error_invalid_phone));
+        } else if (mobileEditText.getText().toString().trim().length() != 10) {
+            mobileEditText.setError(getResources().getString(R.string.error_invalid_phone));
             isPhoneValid = false;
         } else {
             isPhoneValid = true;
         }
-        if (password.getText().toString().trim().isEmpty()) {
-            password.setError(getResources().getString(R.string.error_Empty_Password));
+        if (passwordEditText.getText().toString().trim().isEmpty()) {
+            passwordEditText.setError(getResources().getString(R.string.error_Empty_Password));
             isPasswordValid = false;
         } else {
-            if (!lowerCase.matcher(password.getText().toString().trim()).find()) {
-                password.setError(getResources().getString(R.string.error_LowerCase_Password));
+            if (!lowerCase.matcher(passwordEditText.getText().toString().trim()).find()) {
+                passwordEditText.setError(getResources().getString(R.string.error_LowerCase_Password));
                 isPasswordValid = false;
-            } else if (!digitCase.matcher(password.getText().toString().trim()).find()) {
-                password.setError(getResources().getString(R.string.error_digitCase_Password));
+            } else if (!digitCase.matcher(passwordEditText.getText().toString().trim()).find()) {
+                passwordEditText.setError(getResources().getString(R.string.error_digitCase_Password));
                 isPasswordValid = false;
-            } else if (password.getText().toString().trim().length() < 8) {
-                password.setError(getResources().getString(R.string.length_error));
+            } else if (passwordEditText.getText().toString().trim().length() < 8) {
+                passwordEditText.setError(getResources().getString(R.string.length_error));
                 isPasswordValid = false;
             }
         }
-        if (cpassword.getText().toString().trim().isEmpty()) {
-            cpassword.setError(getResources().getString(R.string.error_Empty_CPassword));
+        if (confirmPasswordEditText.getText().toString().trim().isEmpty()) {
+            confirmPasswordEditText.setError(getResources().getString(R.string.error_Empty_CPassword));
             isPasswordValid = false;
-        } else if (!password.getText().toString().trim().equals(cpassword.getText().toString())) {
-            cpassword.setError(getResources().getString(R.string.error_Cpassword_Match));
+        } else if (!passwordEditText.getText().toString().trim().equals(confirmPasswordEditText.getText().toString())) {
+            confirmPasswordEditText.setError(getResources().getString(R.string.error_Cpassword_Match));
             isPasswordValid = false;
-        } else if (password.getText().toString().trim().equals(cpassword.getText().toString())) {
+        } else if (passwordEditText.getText().toString().trim().equals(confirmPasswordEditText.getText().toString())) {
             isPasswordValid = true;
         }
         if (isNameValid && isEmailValid && isPhoneValid && isPasswordValid) {
-            String email_txt = email.getText().toString().trim();
-            String password_txt = password.getText().toString().trim();
+            String email_txt = emailEditText.getText().toString().trim();
+            String password_txt = passwordEditText.getText().toString().trim();
             final Handler handler = new Handler();
         }
         if (isNameValid && isEmailValid && isPhoneValid && isPasswordValid) {
-            String email_txt = email.getText().toString().trim();
-            String password_txt = password.getText().toString().trim();
+            String email_txt = emailEditText.getText().toString().trim();
+            String password_txt = passwordEditText.getText().toString().trim();
             final Handler handler = new Handler();
 
             firebaseAuth.createUserWithEmailAndPassword(email_txt, password_txt)
@@ -189,7 +207,7 @@ public class SignUpActivity extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
 
                             if (task.isSuccessful()) {
-                                User userInfo = new User(firebaseAuth.getUid(), name.getText().toString().trim(), email_txt, phone.getText().toString().trim());
+                                User userInfo = new User(firebaseAuth.getUid(), nameEditText.getText().toString().trim(), email_txt, mobileEditText.getText().toString().trim());
                                 new FirestoreClass().registerUser(SignUpActivity.this, userInfo);
                                 firebaseAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
                                     @Override
